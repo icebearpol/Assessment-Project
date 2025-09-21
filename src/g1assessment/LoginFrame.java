9305e4a52fe014dc9a3be4269de9ebc454beb2d1
lcm-conrad <sagrado.luisconrad@smctagum.edu.ph>
2025-09-21 23:35:07 +0800

    LOGIN UI
    import flatlaf UI to Login

--- src/g1assessment/LoginFrame.java
+++ src/g1assessment/LoginFrame.java
@@ -4,7 +4,9 @@
  */
 package g1assessment;
 
+import com.formdev.flatlaf.FlatLightLaf;
 import javax.swing.JOptionPane;
+import javax.swing.UIManager;
 
 /**
  *
@@ -157,7 +159,11 @@ public class LoginFrame extends javax.swing.JFrame {
      */
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+                try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (Exception ex) {
+            System.err.println("Failed to initialize LaF");
+        }//<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
